e9b3df549158884de027e32b6002741fe8698916
Mz-Akbar <muhammadzafirulakbar88@gmail.com>
2025-06-22 00:56:40 +0700

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,17 +1,24 @@
-resource "aws_db_instance" "mydbinstance" {
-    engine = "mysql"
-    instance_class = "db.t3.micro"
-    engine_version = "8.0.41"
-    allocated_storage = 20
-    storage_type = "gp2"
-    identifier = "myrds"
-    username = "admin"
-    password = "password123"
-    skip_final_snapshot = true
-    db_name = "mydbs"
+resource "aws_db_subnet_group" "my_db_subnet_group"{
+    name = "my-db-subnet-group"
+    subnet_ids =[aws_subnet.Private1.id, aws_subnet.Private2.id]
 
     tags = {
-        Name = "MyRDSInstance"
-
+        Name = "My DB Subnet Group"
     }
-}  
+
+}
+
+resource "aws_db_instance" "mydb"  {
+    allocated_storage = 20
+    storage_type = "gp2"
+    engine = "mysql"
+    engine_version = "8.0.41"
+    instance_class = "db.t3.micro"
+    db_name = "mydatabase"
+    username = var.db_username
+    password = var.db_password
+    db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
+    vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+    skip_final_snapshot = true
+}
+        
