520e88898938cf5f5e5da146f8448c54aeb3f0ff
Mz-Akbar <muhammadzafirulakbar88@gmail.com>
2025-06-21 22:13:53 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,30 +1,36 @@
 resource "aws_vpc" "MyVPC"{
     cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "MyVpc"
+
+    tags ={
+        Name = "MyVPC"
     }
+
 }
 
-resource "aws_subnet" "PublicSubnet1" {
+resource "aws_subnet" "Public1" {
     vpc_id = aws_vpc.MyVPC.id
     cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
 }
-
-resource "aws_subnet" "PublicSubnet2"{
+  
+resource "aws_subnet" "Public2" {
     vpc_id = aws_vpc.MyVPC.id
     cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
 }
-
-resource "aws_subnet" "PrivateSubnet1" {
+   
+resource "aws_subnet" "Private1" {
     vpc_id = aws_vpc.MyVPC.id
     cidr_block = "10.0.3.0/24"
+    availability_zone = "us-east-1a"
 }
-
-resource "aws_subnet" "PrivateSubnet2"{
+    
+resource "aws_subnet" "Private2" {
     vpc_id = aws_vpc.MyVPC.id
     cidr_block = "10.0.4.0/24"
+    availability_zone = "us-east-1b"
 }
-
+  
 resource "aws_internet_gateway" "Igw"{
     vpc_id = aws_vpc.MyVPC.id
 }
@@ -37,13 +43,13 @@ resource "aws_route_table" "PublicRoutetable" {
     }
 }
 
-resource "aws_route_table_association" "PublicSubnet1" {
-    subnet_id = aws_subnet.PublicSubnet1.id  
+resource "aws_route_table_association" "Public1" {
+    subnet_id = aws_subnet.Public1.id  
     route_table_id = aws_route_table.PublicRoutetable.id
 }
 
-resource "aws_route_table_association" "PublicSubnet2" {
-    subnet_id = aws_subnet.PublicSubnet2.id
+resource "aws_route_table_association" "Public2" {
+    subnet_id = aws_subnet.Public2.id
     route_table_id = aws_route_table.PublicRoutetable.id
 }
 
