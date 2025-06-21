c3bf2cc0ba27ec7e40a3a39892e6c22b8dae8d67
Mz-Akbar <muhammadzafirulakbar88@gmail.com>
2025-06-21 22:14:49 +0700

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,26 +1,54 @@
-resource "aws_s3_bucket" "s3" {
-    bucket = "my-tf-s3-bucket-1050"
+resource "aws_s3_bucket" "static-site"{
+    bucket = "static-site-web-2025"
 
-    tags = {
-        Name = "MyBucket"
-        Environment = "Dev"
+    tags ={
+        Name = "Static_website"
     }
 }
 
+resource "aws_s3_bucket_website_configuration" "static-site"{
+    bucket = aws_s3_bucket.static-site.id
 
-resource "aws_s3_bucket" "B" {
-    bucket = "my-tf-s3-akbar-2007"
+    index_document {
+        suffix = "index.html"
+    }
 
-    tags = {
-        Name = "bucket"
-        Environment = "Dev"
+    error_document {
+        key = "error.html"
     }
 }
 
-resource "aws_s3_bucket" "c" {
-    bucket = "my-tf-s3-ppp-210"
-    tags = {
-        Name = "ember"
-        Environment = "Dev"
+resource "aws_s3_bucket_public_access_block" "static-site"{
+    bucket = aws_s3_bucket.static-site.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "get_and_put"{
+    statement {
+        principals {
+            type = "AWS"
+            identifiers = ["arn:aws:iam::663861047640:user/lks-user"]
+        }
+
+        actions = [
+            "s3:GetObject",
+            "s3:PutObject"
+        ]
+
+        resources = [
+            aws_s3_bucket.static-site.arn,
+            "${aws_s3_bucket.static-site.arn}/*"
+
+
+        ]
+        
     }
+}
+
+resource "aws_s3_bucket_policy" "web_policy" {
+  bucket = aws_s3_bucket.static-site.id
+  policy = data.aws_iam_policy_document.get_and_put.json
 }
