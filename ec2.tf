fe6478c23b4f001bde5de04608441757e7cfb500
Mz-Akbar <muhammadzafirulakbar88@gmail.com>
2025-06-21 22:14:23 +0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,35 @@
-resource "aws_instance" "MyEC2" {
+# EC2 Pertama
+resource "aws_instance" "ec2_1" { # EC2 Instance
     ami = "ami-09e6f87a47903347c"
     instance_type = "t2.micro"
-    subnet_id = "subnet-0e838cdb2a6d47545"
-    key_name = "Server web"
+    subnet_id = aws_subnet.Public1.id
+    vpc_security_group_ids = [aws_security_group.ec2_sg.id]  # Menghubungkan SG ke EC2
+    key_name = aws_key_pair.genereted_key.key_name           # Menghubungkan KeyPair ke EC2
+    associate_public_ip_address = true
+
     tags = {
-        Name = "Myec2Instacne"
+        Name = "EC2-1"
     }
 }
+
+# EC2 Kedua
+resource "aws_instance" "ec2_2" { # EC2 Instance
+    ami = "ami-09e6f87a47903347c"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.Public2.id
+    vpc_security_group_ids = [aws_security_group.ec2_sg.id]  # Menghubungkan SG ke EC2
+    key_name = aws_key_pair.genereted_key.key_name           # Menghubungkan KeyPair ke EC2
+    associate_public_ip_address = true
+
+    tags = {
+        Name = "EC2-2"
+    }
+}
+
+
+
+
+
+
+
+
